c196d927f486069d3e01b08445b36de65e472e4a
Christopher Carter <christopher.carter@student.fairfield.edu>
2017-11-26 17:00:50 -0500

    Finished cleaning up merge into master
    Cleaned up merge into master, fixed the player movement class since this has conflicts.  Everything else looked clean.

---
+++ Game/Pathfinder Project/Assets/Scripts/Pickup Behavior/PaintPickUpBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintPickUpBehavior : MonoBehaviour
+{
+    public PickupEnum Pickup = PickupEnum.None;
+    private Player _Player = GameObject.FindGameObjectWithTag("Player").GetComponent(typeof(Player)) as Player;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Player player = GameObject.FindGameObjectWithTag("Player").GetComponent(typeof(Player)) as Player;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            ApplyPickup(collider, this.Pickup);            
+        }
+    }
+
+    void ApplyPickup(Collider collider, PickupEnum pickup)
+    {
+        if (collider.tag == "Player")
+        {   
+            _Player.CurrentPickup = pickup;
+
+            // Update the color of the player
+            collider.transform.GetComponent<Renderer>().material.color = GetColorForPickup(pickup);
+        }
+    }
+
+    void ApplyPickupColor(Collider collider, PickupEnum pickup)
+    {
+    }
+
+    public static Color GetColorForPickup(PickupEnum pickup)
+    {
+        Color color = Color.white;
+
+        switch (pickup)
+        {
+            case PickupEnum.None:
+                color = Color.white;
+                break;
+            case PickupEnum.Speed:
+                color = Color.red;
+                break;
+            default:
+                color = Color.white;
+                break;
+        }
+
+        return color;
+    }
+}
