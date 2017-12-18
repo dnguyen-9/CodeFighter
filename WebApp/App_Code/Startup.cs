4ddbfce4e666029f02029ee29469fa8b10072d8c
Maddy4577 <madanmohanreddy.veerarouth@student.fairfield.edu>
2017-12-18 12:06:45 -0500

    Add files via upload

---
+++ WebApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeFighters.Startup))]
+namespace CodeFighters
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
